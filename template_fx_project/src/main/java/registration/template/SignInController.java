8933d195f5f142e16f98bb959cb7fe0c8fe01c32
Kevin Karimi <kevinrkarimi@gmail.com>
2025-04-08 18:41:17 -0700

    sign in controller class

---
+++ template_fx_project/src/main/java/registration/template/SignInController.java
@@ -0,0 +1,25 @@
+package registration.template;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SignInController {
+
+ private Stage stage;
+ private Scene scene;
+ private Parent root;
+ 
+ public void switchToFlightSearch(ActionEvent event) throws IOException {
+  root = FXMLLoader.load(getClass().getResource("FlightSearch.fxml"));
+  stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+  scene = new Scene(root);
+  stage.setScene(scene);
+  stage.show();
+ }
+}
